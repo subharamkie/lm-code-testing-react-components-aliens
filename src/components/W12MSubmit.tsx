cb3e6d4f6d5f841443a39715605ba924bbbf4651
Subha <subharamkie@gmail.com>
2023-12-16 17:18:03 +0000

    Fix:Fixed the onSubmit call for the button

--- src/components/W12MSubmit.tsx
+++ src/components/W12MSubmit.tsx
@@ -1,11 +1,11 @@
 interface W12MSubmitProps{
     id:string;
-    onSubmitFn:(value:string)=>void;
+    onSubmitFn :() =>void;
 }
 const W12MSubmit:React.FC<W12MSubmitProps>=({id,onSubmitFn})=>{
     return (
     <div className="mt-5">
-        <button id="submit" onClick={()=>onSubmitFn}>
+        <button id="submit" onClick={()=>onSubmitFn()}>
             Submit Form
         </button>
 	</div>
