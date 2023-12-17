55b9a07ba8a41fa485c553f2ddf7a9a156ac9efb
Subha <subharamkie@gmail.com>
2023-12-17 18:33:11 +0000

    Feat:Added validation for dropdown

--- src/components/W12MSelect.tsx
+++ src/components/W12MSelect.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import ErrorMessage from "./W12MErrorMessage";
+import { validateInputElement } from "../validate/validate_inputElement";
 
 export interface W12MSelectProps{
     id:string;
@@ -8,17 +11,30 @@ export interface W12MSelectProps{
 
 }
 const W12MSelect:React.FC<W12MSelectProps> = ({id,onChangeFn,value,options,label}) =>{
+    const [errorMessage,setErrorMessage] = useState<string[]>([]);
+    const handleChange = (value:string) => {
+        //const inputValue = e.target.value;
+        const errorMessage = validateInputElement(id, value);
+    
+        // Assuming you have some way to handle and display the validation errors
+        console.log("Validation errors:", errorMessage);
+        setErrorMessage(errorMessage);
+    
+        onChangeFn(value);
+      };
+    
     
     return (
         <div className="flex flex-col w-full gap-2">
             <select id={id} value={value}
-            onChange={(e) => onChangeFn(e.target.value)}> 
+            onChange={(e) => handleChange(e.target.value)}> 
                 <option>{label}</option>
                 {options.map((i)=>{
                     return <option key={i.value} value={i.value}>{i.label}</option>
                 })}
                 
             </select>
+            {errorMessage.length > 0 && <ErrorMessage message={errorMessage} />}
         </div>
     )
 } 
--- src/validate/validate_inputElement.tsx
+++ src/validate/validate_inputElement.tsx
@@ -1,3 +1,4 @@
+
 export const validateInputElement : (value : string,id:string) => string[] = (id,value) => {
 	//check which type of input element is being passed by using the id, then apply the validation rules
 	const errors:string[] = [];
@@ -7,16 +8,42 @@ export const validateInputElement : (value : string,id:string) => string[] = (id
 			//len 3-23,no number/special character
 			if(value.length <3 || value.length >23){
 				errors.push('Length has to be between 3-23 characters');
-			}/*
-			if(value.match(/[^a-zA-Z0-9]+/)){
+			}
+			if(value.match(/[^a-zA-Z]+/)){
 				errors.push('No numbers or special characters allowed!');
-			}*/
+			}
 			return errors;
 		
-		case 'planet':break;
-		case'beings':break;
-		case 'mathQn':break;
-		case 'reason':break;
+		case 'planet':
+			if(value.length <2 || value.length >49){
+				errors.push('Length has to be between 2-49 characters');
+			}
+			
+			if(value.match(/[^a-zA-Z0-9]+/)){
+				errors.push('No special characters allowed!');
+			}
+			return errors;
+		case'beings':
+			if(value.match(/[^0-9]/)){
+				errors.push('Only numbers above 1,000,000,000 are allowed');
+			}
+			if(parseInt(value) < 1000000000 ){
+				errors.push('Number must be above 1,000,000,000');
+			}
+			return errors;
+		case 'mathQn':
+			if(value !== '4'){
+				errors.push('Answer has to be 2+2');
+			}
+			if(value === ''){
+				errors.push('An option must be selected');
+			}
+			return errors;
+		case 'reason':
+			if(value.length <17 || value.length > 153){
+				errors.push('Length has to be between 17-153 characters.');
+			}
+			return errors;
 	}
 	return errors;
 };
